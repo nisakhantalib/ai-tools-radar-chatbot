dcc5154024f80a814468cc65a05d1f3e2ce906db
Fardin <faa35@sfu.ca>
2025-01-04 01:49:18 -0800

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+Based on this course "Build and Deploy a RAG Chatbot with JavaScript, LangChain.js, Next.js, Vercel, OpenAI" https://www.youtube.com/watch?v=d-VKYF4Zow0
---
+++ scripts/loadDb.ts
@@ -0,0 +1,98 @@
+import { DataAPIClient } from "@datastax/astra-db-ts";
+import { PuppeteerWebBaseLoader } from "@langchain/community/document_loaders/web/puppeteer";
+import OpenAI from "openai";
+
+import { RecursiveCharacterTextSplitter } from "langchain/text_splitter";
+
+import "dotenv/config";
+
+type SimilarityMetric = "dot_product" | "cosine" | "euclidean";
+
+// set up environment variables
+const {
+  ASTRA_DB_API_ENDPOINT,
+  ASTRA_DB_APPLICATION_TOKEN,
+  ASTRA_DB_NAMESPACE,
+  ASTRA_DB_COLLECTION,
+  OPENAI_API_KEY,
+} = process.env;
+
+const openai = new OpenAI({ apiKey: OPENAI_API_KEY });
+
+// define which website to scrape
+const f1Data = [
+  "https://www.ufc.com/rankings",
+  "https://en.wikipedia.org/wiki/List_of_UFC_events",
+  "https://en.wikipedia.org/wiki/Ultimate_Fighting_Championship",
+  "https://en.wikipedia.org/wiki/List_of_UFC_events",
+  "https://en.wikipedia.org/wiki/UFC_308",
+];
+
+// Comment: Not conforming to TS strictness
+const client = new DataAPIClient(ASTRA_DB_APPLICATION_TOKEN);
+const db = client.db(ASTRA_DB_API_ENDPOINT, { namespace: ASTRA_DB_NAMESPACE });
+
+// set splitter + options
+const splitter = new RecursiveCharacterTextSplitter({
+  chunkSize: 512,
+  chunkOverlap: 100,
+});
+
+// create collection on datastrax from datastax API
+const createCollection = async (
+  similarityMetric: SimilarityMetric = "dot_product"
+) => {
+  const res = await db.createCollection(ASTRA_DB_COLLECTION, {
+    vector: {
+      dimension: 1536,
+      metric: similarityMetric,
+    },
+  });
+
+  console.log(res);
+};
+
+// load sample data into collection
+const loadSampleData = async () => {
+  const collection = await db.collection(ASTRA_DB_COLLECTION);
+  for await (const url of f1Data) {
+    const content = await scrapePage(url);
+    const chunks = await splitter.splitText(content);
+    for await (const chunk of chunks) {
+      const embedding = await openai.embeddings.create({
+        model: "text-embedding-3-small",
+        input: chunk,
+        encoding_format: "float",
+      });
+
+      const vector = embedding.data[0].embedding;
+
+      const res = await collection.insertOne({
+        $vector: vector,
+        text: chunk,
+      });
+
+      console.log(res);
+    }
+  }
+};
+
+// scrape webpage
+const scrapePage = async (url: string) => {
+  const loader = new PuppeteerWebBaseLoader(url, {
+    launchOptions: {
+      headless: true,
+    },
+    gotoOptions: {
+      waitUntil: "domcontentloaded",
+    },
+    evaluate: async (page, browser) => {
+      const result = await page.evaluate(() => document.body.innerHTML);
+      await browser.close();
+      return result;
+    },
+  });
+  return ( await loader.scrape())?.replace(/<[^>]*>?/gm, "");
+};
+
+createCollection().then(() => loadSampleData());
